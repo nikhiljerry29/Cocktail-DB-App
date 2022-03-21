f32b8e66d4a16d448dc792a66a4f94244580c035
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-03-21 18:20:44 +0530

    Update cocktail info

--- src/components/cocktail/CocktailInfo.jsx
+++ src/components/cocktail/CocktailInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiCategoryAlt } from "react-icons/bi";
 import { FaGlassWhiskey, FaWineGlassAlt } from "react-icons/fa";
 import { LazyLoadImage } from "react-lazy-load-image-component";
@@ -6,6 +6,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import Badge from "../shared/Badge";
 
 function CocktailInfo({ cocktail }) {
+   const [ingredientList, setIngredientList] = useState([]);
    const {
       idDrink,
       strDrink,
@@ -14,29 +15,42 @@ function CocktailInfo({ cocktail }) {
       strGlass,
       strInstructions,
       strDrinkThumb,
-      strIngredient1,
-      strMeasure1,
    } = cocktail;
-   // strIngredient2,
-   // strIngredient3,
-   // strMeasure2,
-   // strMeasure3,
+
+   useEffect(() => {
+      const lists = [];
+      for (let index = 1; index <= 15; index++) {
+         if (
+            cocktail[`strIngredient${index}`] !== null &&
+            cocktail[`strIngredient${index}`] !== ""
+         ) {
+            lists.push({
+               id: `ingredient${index}`,
+               ingredient: cocktail[`strIngredient${index}`],
+               measure: cocktail[`strMeasure${index}`],
+            });
+         }
+      }
+
+      console.log(lists);
+      setIngredientList(lists);
+   }, [cocktail]);
 
    return (
       <section
          id={idDrink}
-         className='p-5 flex flex-col lg:flex-row gap-5 md:justify-center '
+         className='p-5 flex flex-col lg:flex-row gap-5 md:justify-center'
       >
          {strDrinkThumb && (
             <LazyLoadImage
                effect='blur'
-               className='w-full h-72 md:h-96 lg:h-auto lg:max-w-md  object-cover rounded shadow-lg'
+               className='w-full h-72 md:h-96 lg:h-auto lg:max-w-lg object-cover rounded-lg shadow-xl'
                alt={strDrink}
                src={strDrinkThumb}
             />
          )}
          <div className='lg:max-w-md'>
-            <h1 className='text-gray-900 text-2xl font-bold tracking-wide capitalize'>
+            <h1 className='text-gray-900 text-3xl font-bold tracking-wide capitalize'>
                {strDrink}
             </h1>
             <p className='text-gray-700 text-md my-3'>{strInstructions}</p>
@@ -54,13 +68,20 @@ function CocktailInfo({ cocktail }) {
                   {strGlass}
                </Badge>
             </div>
-            <h3>Ingredients</h3>
-            <div>
-               <div>
-                  <p>{strIngredient1}</p>
-                  <p>{strMeasure1}</p>
-               </div>
-            </div>
+
+            <h3 className='my-3 text-lg font-semibold'>Ingredients</h3>
+            <ul>
+               {ingredientList.map((item) => (
+                  <li
+                     className='flex flex-row justify-between p-3 bg-zinc-50 my-2 hover:bg-zinc-200 hover:text-zinc-900 transition duration-200 ease-in-out'
+                     key={item.id}
+                     id={item.id}
+                  >
+                     <span>{item.ingredient}</span>
+                     <span>{item.measure}</span>
+                  </li>
+               ))}
+            </ul>
          </div>
       </section>
    );
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
             <div className='flex items-center'>
                <GiGlassShot className='text-4xl text-slate-600' />
                <NavLink
-                  to='/about'
+                  to='/'
                   className='font-stylescript text-gray-900 text-3xl font-bold pt-1 inline'
                >
                   Sip Bar.
